f6b5bccabaca266710479b2ce7d02af133970835
Devin Norgarb <dnorgarb@gmail.com>
2022-02-06 22:42:11 +0200

    first commit

---
+++ prometheus-cpp/core/tests/check_names_test.cc
@@ -0,0 +1,25 @@
+#include "prometheus/check_names.h"
+
+#include <gmock/gmock.h>
+
+namespace prometheus {
+namespace {
+
+TEST(CheckNamesTest, empty_metric_name) { EXPECT_FALSE(CheckMetricName("")); }
+TEST(CheckNamesTest, good_metric_name) {
+  EXPECT_TRUE(CheckMetricName("prometheus_notifications_total"));
+}
+TEST(CheckNamesTest, reserved_metric_name) {
+  EXPECT_FALSE(CheckMetricName("__some_reserved_metric"));
+}
+TEST(CheckNamesTest, malformed_metric_name) {
+  EXPECT_FALSE(CheckMetricName("fa mi ly with space in name or |"));
+}
+TEST(CheckNamesTest, empty_label_name) { EXPECT_FALSE(CheckLabelName("")); }
+TEST(CheckNamesTest, good_label_name) { EXPECT_TRUE(CheckLabelName("type")); }
+TEST(CheckNamesTest, reserved_label_name) {
+  EXPECT_FALSE(CheckMetricName("__some_reserved_label"));
+}
+
+}  // namespace
+}  // namespace prometheus
